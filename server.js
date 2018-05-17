64b6574d379a1e9c12202708085e31fdb53932ce
Nic Ferrier <nic@ferrier.me.uk>
2018-05-17 21:59:10 +0100

    basically working

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { spawn } = require("child_process");
-const fs = require('./fsasync.js');
+const path = require('path');
+const fs = require('fs');
+fs.promises = require('./fsasync.js').promises;
 
 const things = {
     "du_of_home_dir": "du -h /home/nicferrier",
@@ -7,22 +9,43 @@ const things = {
     "uptime": "uptime"
 }
 
+Array.prototype.forEachAsync = async function (fn) {
+    for (let t of this) { await fn(t) }
+};
+
 function eventToHappen(eventFn) {
     return new Promise((resolve, reject) => {
         eventFn(resolve);
     });
 }
 
-function commonyQueue(logDir) {
+async function commonyQueue(logDir) {
     let runDate = new Date();
-    let runDateStr = runDate.getFullYear()
-        + runDate.getMonth() + 1
-        + runDate.getDate()
-        + runDate.getHours()
-        + runDate.getMinutes();
-    
+    let runDateStr = path.join(
+        logDir,
+        runDate.getFullYear()
+            + "-" + ("0" + (runDate.getMonth() + 1)).substr(-2),
+        "" + runDate.getDate(),
+        ("0" + runDate.getHours()).substr(-2)
+            + ("0" + runDate.getMinutes()).substr(-2),
+        "placeholder"
+    );
+
+    let directory = path.dirname(runDateStr);
+    let dirParts = directory.split(path.sep);
+    async function mkdirRecur (mkdir, rest) {
+        await fs.promises.mkdir(mkdir).catch(e => console.log("error!", e));
+        if (rest.length > 0) {
+            let next = path.join(mkdir, rest[0]);
+            let nextRest = rest.slice(1);
+            await mkdirRecur(next, nextRest);
+        }
+    };
+
+    await mkdirRecur(dirParts[0], dirParts.slice(1));
     Object.keys(things).forEach(async dirName => {
-        let out = fs.createWriteStream(logDir + "/" + dirName + "-" + runDateStr);
+        let fileName = path.join(path.dirname(runDateStr), dirName + ".txt");
+        let out = fs.createWriteStream(fileName);
         let script = things[dirName];
         child = spawn("/bin/bash", ["-c", script]);
         child.stderr.pipe(out);
@@ -36,6 +59,6 @@ function commonyQueue(logDir) {
 let minutes15 = 1000 * 60 * 15;
 //setInterval(commonyQueue, minutes15);
 
-commonyQueue();
+commonyQueue("logdir");
 
 // End
